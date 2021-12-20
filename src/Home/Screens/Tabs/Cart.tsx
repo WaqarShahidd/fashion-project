8a9b0aa7aa9aed9eb690725af606953d8261aa5c
amaso2001 <hamzabilalbaig@gmail.com>
2021-12-20 23:01:24 +0500

    allhumdililah

--- src/Home/Screens/Tabs/Cart.tsx
+++ src/Home/Screens/Tabs/Cart.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import { Text, View, StyleSheet } from "react-native";
-import Categories from "../SearchComponents/CategoriesM";
-import ShoppingModal from "../SearchComponents/ShoppingModal";
-
+import CartItmeList from "../../Components/CartItemList";
+import CartTotal from "../../Components/CartTotal";
 interface CartProps {}
 
 const Cart = (props: CartProps) => {
   return (
     <View style={styles.container}>
-      <Text>CART</Text>
+      <CartItmeList />
+      <CartTotal />
     </View>
   );
 };
@@ -18,6 +18,6 @@ export default Cart;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 50,
+    marginTop: 0,
   },
 });
